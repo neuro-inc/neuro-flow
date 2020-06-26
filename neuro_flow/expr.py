0f5d17619ab23490cc8bb80ebad5c6a9ff7490c7
Andrew Svetlov <andrew.svetlov@gmail.com>
2020-06-26 13:14:18 +0300

    Support literals

--- neuro_flow/expr.py
+++ neuro_flow/expr.py
@@ -3,20 +3,28 @@
 # ${{ <expression> }}
 
 import dataclasses
-from typing import List, Sequence, Tuple
+from ast import literal_eval
+from typing import Any, List, Sequence, Tuple
 
 from funcparserlib.lexer import Token, make_tokenizer
-from funcparserlib.parser import a, finished, many, maybe, oneplus, skip, some
+from funcparserlib.parser import Parser, a, finished, many, maybe, oneplus, skip, some
 
 
 TOKENS = [
     ("TMPL", (r"\$\{\{|\}\}",)),
     ("SPACE", (r"[ \t]+",)),
+    ("NONE", (r"None",)),
+    ("BOOL", (r"True|False",)),
+    ("REAL", (r"-?[0-9]+\.[0-9]*([Ee][+\-]?[0-9]+)*",)),
+    ("EXP", (r"-?[0-9]+\.[0-9]*([Ee][+\-]?[0-9]+)*[+\-]?e[0-9]+",)),
+    ("HEX", (r"0[xX][0-9a-fA-F_]+",)),
+    ("OCT", (r"0[oO][0-7_]+",)),
+    ("BIN", (r"0[bB][0-1_]+",)),
+    ("INT", (r"-?[0-9][0-9_]*",)),
+    ("STR", (r"'[^']*'",)),
     ("NAME", (r"[A-Za-z_][A-Za-z_0-9\-]*",)),
     ("DOT", (r"\.",)),
     ("ANY", (r".",)),
-    # ('REAL', (r'[0-9]+\.[0-9]*([Ee][+\-]?[0-9]+)*',)),
-    # ('INT', (r'[0-9]+',)),
 ]
 
 
@@ -25,6 +33,13 @@ tokenize = make_tokenizer(TOKENS)
 
 def tokval(tok: Token) -> str:
     return tok.value
+
+
+def literal(toktype: str) -> Parser:
+    def f(tok: Token) -> Any:
+        return literal_eval(tokval(tok))
+
+    return some(lambda tok: tok.type == toktype) >> f
 
 
 @dataclasses.dataclass(frozen=True)
@@ -51,11 +66,33 @@ CLOSE_TMPL = skip(a(Token("TMPL", "}}")))
 
 NOT_TMPL = some(lambda tok: tok.type != "TMPL") >> tokval
 
+REAL = literal("REAL")
+
+EXP = literal("EXP")
+
+INT = literal("INT")
+
+HEX = literal("HEX")
+
+OCT = literal("OCT")
+
+BIN = literal("BIN")
+
+BOOL = literal("BOOL")
+
+STR = literal("STR")
+
+NONE = literal("NONE")
+
+LITERAL = REAL | EXP | INT | HEX | OCT | BIN | BOOL | STR | NONE
+
 NAME = some(lambda tok: tok.type == "NAME") >> tokval
 
 LOOKUP = NAME + many(DOT + NAME) >> make_lookup
 
-TMPL = OPEN_TMPL + skip(maybe(SPACE)) + LOOKUP + skip(maybe(SPACE)) + CLOSE_TMPL
+EXPR = LITERAL | LOOKUP
+
+TMPL = OPEN_TMPL + skip(maybe(SPACE)) + EXPR + skip(maybe(SPACE)) + CLOSE_TMPL
 
 TEXT = oneplus(NOT_TMPL) >> (lambda arg: Text("".join(arg)))
 
--- tests/unit/test_expr_parse.py
+++ tests/unit/test_expr_parse.py
@@ -47,6 +47,70 @@ def test_tmpl_false5() -> None:
         finish(TMPL).parse(list(tokenize("name }}")))
 
 
+def test_tmpl_literal_none() -> None:
+    assert finish(TMPL).parse(list(tokenize("${{ None }}"))) is None
+
+
+def test_tmpl_literal_real() -> None:
+    assert 12.34 == finish(TMPL).parse(list(tokenize("${{ 12.34 }}")))
+
+
+def test_tmpl_literal_exp() -> None:
+    assert -12.34e-21 == finish(TMPL).parse(list(tokenize("${{ -12.34e-21 }}")))
+
+
+def test_tmpl_literal_int1() -> None:
+    assert 1234 == finish(TMPL).parse(list(tokenize("${{ 1234 }}")))
+
+
+def test_tmpl_literal_int2() -> None:
+    assert 1234 == finish(TMPL).parse(list(tokenize("${{ 12_34 }}")))
+
+
+def test_tmpl_literal_int3() -> None:
+    assert -1234 == finish(TMPL).parse(list(tokenize("${{ -1234 }}")))
+
+
+def test_tmpl_literal_hex1() -> None:
+    assert 0x12AB == finish(TMPL).parse(list(tokenize("${{ 0x12ab }}")))
+
+
+def test_tmpl_literal_hex2() -> None:
+    assert 0x12AB == finish(TMPL).parse(list(tokenize("${{ 0X12_ab }}")))
+
+
+def test_tmpl_literal_oct1() -> None:
+    assert 0o1234 == finish(TMPL).parse(list(tokenize("${{ 0o1234 }}")))
+
+
+def test_tmpl_literal_oct2() -> None:
+    assert 0o1234 == finish(TMPL).parse(list(tokenize("${{ 0O12_34 }}")))
+
+
+def test_tmpl_literal_bin1() -> None:
+    assert 0b0110 == finish(TMPL).parse(list(tokenize("${{ 0b0110 }}")))
+
+
+def test_tmpl_literal_bin2() -> None:
+    assert 0b0110 == finish(TMPL).parse(list(tokenize("${{ 0B01_10 }}")))
+
+
+def test_tmpl_literal_bool1() -> None:
+    assert finish(TMPL).parse(list(tokenize("${{ True }}"))) is True
+
+
+def test_tmpl_literal_bool2() -> None:
+    assert finish(TMPL).parse(list(tokenize("${{ False }}"))) is False
+
+
+def test_tmpl_literal_str1() -> None:
+    assert "str" == finish(TMPL).parse(list(tokenize("${{ 'str' }}")))
+
+
+def test_tmpl_literal_str2() -> None:
+    assert "abc\tdef" == finish(TMPL).parse(list(tokenize("${{ 'abc\tdef' }}")))
+
+
 def test_text_ok() -> None:
     assert Text("some text") == finish(TEXT).parse(list(tokenize("some text")))
 
