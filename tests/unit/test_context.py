ae618a760fa0d4502ff84e0833cf470e5be67759
Andrew Svetlov <andrew.svetlov@gmail.com>
2020-07-15 17:40:51 +0300

    Add tests for job context

--- tests/unit/test_context.py
+++ tests/unit/test_context.py
@@ -1,4 +1,5 @@
 import pathlib
+import pytest
 from yarl import URL
 
 from neuro_flow.context import Context, NotAvailable
@@ -97,3 +98,37 @@ async def test_defaults(assets: pathlib.Path) -> None:
     assert ctx.defaults.workdir == RemotePath("/global/dir")
     assert ctx.defaults.life_span == 100800.0
     assert ctx.defaults.preset == "cpu-large"
+
+
+async def test_job_root_ctx(assets: pathlib.Path) -> None:
+    workspace = assets / "jobs-full"
+    config_file = workspace / ".neuro" / "jobs.yml"
+    flow = parse_interactive(workspace, config_file)
+    ctx = await Context.create(flow)
+    with pytest.raises(NotAvailable):
+        ctx.job
+
+
+async def test_job(assets: pathlib.Path) -> None:
+    workspace = assets / "jobs-full"
+    config_file = workspace / ".neuro" / "jobs.yml"
+    flow = parse_interactive(workspace, config_file)
+    ctx = await Context.create(flow)
+
+    ctx2 = await ctx.with_job("test_a")
+    assert ctx2.job.id == "test_a"
+    assert ctx2.job.title == "Job title"
+    assert ctx2.job.name == "job-name"
+    assert ctx2.job.image == "image:banana"
+    assert ctx2.job.preset == "cpu-small"
+    assert ctx2.job.http_port == 8080
+    assert not ctx2.job.http_auth
+    assert ctx2.job.entrypoint == "bash"
+    assert ctx2.job.cmd == "echo abc"
+    assert ctx2.job.workdir == RemotePath('/local/dir')
+    assert ctx2.job.volumes == ['storage:dir:/var/dir:ro', 'storage:dir:/var/dir:ro']
+    assert ctx2.job.tags == {'tag-1', 'tag-2', 'tag-a', 'tag-b'}
+    assert ctx2.job.life_span == 10500.0
+    assert ctx2.job.port_forward == ['2211:22']
+    assert ctx2.job.detach
+    assert ctx2.job.browse
