7272a82002a24bf094e584fb17543073f434b3be
Andrew Svetlov <andrew.svetlov@gmail.com>
2020-09-07 15:32:06 +0300

    Support $[[ expr ]] syntax as well as ${{ expr }} (#49)

--- tests/unit/test_tokenizer.py
+++ tests/unit/test_tokenizer.py
@@ -28,7 +28,7 @@ def test_non_template_with_unknown_chars() -> None:
     ] == list(tokenize("abc!jik", Pos(0, 0, LocalPath("<test>"))))
 
 
-def test_template() -> None:
+def test_template_curly() -> None:
     assert [
         Token(
             "TEXT",
@@ -79,3 +79,56 @@ def test_template() -> None:
             Pos(0, 29, LocalPath("<test>")),
         ),
     ] == list(tokenize("abc ${{ job.job_id.name }}jik", Pos(0, 0, LocalPath("<test>"))))
+
+
+def test_template_square() -> None:
+    assert [
+        Token(
+            "TEXT",
+            "abc ",
+            Pos(0, 0, LocalPath("<test>")),
+            Pos(0, 4, LocalPath("<test>")),
+        ),
+        Token(
+            "LTMPL2",
+            "$[[",
+            Pos(0, 4, LocalPath("<test>")),
+            Pos(0, 7, LocalPath("<test>")),
+        ),
+        Token(
+            "NAME",
+            "job",
+            Pos(0, 8, LocalPath("<test>")),
+            Pos(0, 11, LocalPath("<test>")),
+        ),
+        Token(
+            "DOT", ".", Pos(0, 11, LocalPath("<test>")), Pos(0, 12, LocalPath("<test>"))
+        ),
+        Token(
+            "NAME",
+            "job_id",
+            Pos(0, 12, LocalPath("<test>")),
+            Pos(0, 18, LocalPath("<test>")),
+        ),
+        Token(
+            "DOT", ".", Pos(0, 18, LocalPath("<test>")), Pos(0, 19, LocalPath("<test>"))
+        ),
+        Token(
+            "NAME",
+            "name",
+            Pos(0, 19, LocalPath("<test>")),
+            Pos(0, 23, LocalPath("<test>")),
+        ),
+        Token(
+            "RTMPL2",
+            "]]",
+            Pos(0, 24, LocalPath("<test>")),
+            Pos(0, 26, LocalPath("<test>")),
+        ),
+        Token(
+            "TEXT",
+            "jik",
+            Pos(0, 26, LocalPath("<test>")),
+            Pos(0, 29, LocalPath("<test>")),
+        ),
+    ] == list(tokenize("abc $[[ job.job_id.name ]]jik", Pos(0, 0, LocalPath("<test>"))))
