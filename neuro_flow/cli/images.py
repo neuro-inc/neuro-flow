e62cfc13448ea6457e35875f8747c07c8fa2eed6
Roman Skurikhin <romasku135@gmail.com>
2020-10-20 16:29:01 +0300

    Fix ConfigDir -> Root migration

--- neuro_flow/cli/images.py
+++ neuro_flow/cli/images.py
@@ -1,20 +1,20 @@
 import click
 
 from neuro_flow.cli.click_types import LIVE_IMAGE_OR_ALL
+from neuro_flow.cli.root import Root
 from neuro_flow.cli.utils import argument, wrap_async
 from neuro_flow.live_runner import LiveRunner
-from neuro_flow.parser import ConfigDir
 
 
 @click.command()
 @argument("image", type=LIVE_IMAGE_OR_ALL)
 @wrap_async()
-async def build(config_dir: ConfigDir, image: str) -> None:
+async def build(root: Root, image: str) -> None:
     """Build an image.
 
     Assemble the IMAGE remotely and publish it.
     """
-    async with LiveRunner(config_dir) as runner:
+    async with LiveRunner(root.config_dir) as runner:
         if image == "ALL":
             await runner.build_all()
         else:
--- neuro_flow/cli/storage.py
+++ neuro_flow/cli/storage.py
@@ -1,20 +1,20 @@
 import click
 
 from neuro_flow.cli.click_types import LIVE_VOLUME_OR_ALL
+from neuro_flow.cli.root import Root
 from neuro_flow.cli.utils import argument, wrap_async
 from neuro_flow.live_runner import LiveRunner
-from neuro_flow.parser import ConfigDir
 
 
 @click.command()
 @argument("volume", type=LIVE_VOLUME_OR_ALL)
 @wrap_async()
-async def upload(config_dir: ConfigDir, volume: str) -> None:
+async def upload(root: Root, volume: str) -> None:
     """Upload volume.
 
     Upload local files to remote for VOLUME,
     use `upload ALL` for uploading all volumes."""
-    async with LiveRunner(config_dir) as runner:
+    async with LiveRunner(root.config_dir) as runner:
         if volume != "ALL":
             await runner.upload(volume)
         else:
@@ -24,12 +24,12 @@ async def upload(config_dir: ConfigDir, volume: str) -> None:
 @click.command()
 @argument("volume", type=LIVE_VOLUME_OR_ALL)
 @wrap_async()
-async def download(config_dir: ConfigDir, volume: str) -> None:
+async def download(root: Root, volume: str) -> None:
     """Download volume.
 
     Download remote files to local for VOLUME,
     use `download ALL` for downloading all volumes."""
-    async with LiveRunner(config_dir) as runner:
+    async with LiveRunner(root.config_dir) as runner:
         if volume != "ALL":
             await runner.download(volume)
         else:
@@ -39,12 +39,12 @@ async def download(config_dir: ConfigDir, volume: str) -> None:
 @click.command()
 @argument("volume", type=LIVE_VOLUME_OR_ALL)
 @wrap_async()
-async def clean(config_dir: ConfigDir, volume: str) -> None:
+async def clean(root: Root, volume: str) -> None:
     """Clean volume.
 
     Clean remote files on VOLUME,
     use `clean ALL` for cleaning up all volumes."""
-    async with LiveRunner(config_dir) as runner:
+    async with LiveRunner(root.config_dir) as runner:
         if volume != "ALL":
             await runner.clean(volume)
         else:
@@ -53,7 +53,7 @@ async def clean(config_dir: ConfigDir, volume: str) -> None:
 
 @click.command()
 @wrap_async()
-async def mkvolumes(config_dir: ConfigDir) -> None:
+async def mkvolumes(root: Root) -> None:
     """Create all remote folders for volumes."""
-    async with LiveRunner(config_dir) as runner:
+    async with LiveRunner(root.config_dir) as runner:
         await runner.mkvolumes()
