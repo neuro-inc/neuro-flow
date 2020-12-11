ef723d52378c98e72a0b8a589f742ed3ae9cb603
Andrew Svetlov <andrew.svetlov@gmail.com>
2020-12-11 23:05:43 +0200

    Bump

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     python_requires=">=3.6.0",
     include_package_data=True,
     install_requires=[
-        "neuro-cli>=20.12.11b5",
+        "neuro-cli>=20.12.11b6",
         "pyyaml>=5.3",
         "funcparserlib>=0.3",
         'dataclasses>=0.5; python_version<"3.7"',
