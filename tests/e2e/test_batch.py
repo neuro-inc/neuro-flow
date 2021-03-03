1904cef4848a05cd6d69e8c5d0987cbc9e45a7be
Andrew Svetlov <andrew.svetlov@gmail.com>
2021-03-03 17:25:17 +0200

    Fix test

--- tests/e2e/test_batch.py
+++ tests/e2e/test_batch.py
@@ -8,13 +8,17 @@ from tests.e2e.conftest import RunCLI
 
 def test_seq_batch(run_cli: RunCLI) -> None:
     random_text = secrets.token_hex(20)
-    captured = run_cli(["bake", "seq", "--param", "token", random_text])
+    captured = run_cli(
+        ["bake", "--local-executor", "seq", "--param", "token", random_text]
+    )
 
     assert f"task_b_out: {random_text}" in captured.out
 
     # Now test the cache:
 
-    captured = run_cli(["bake", "seq", "--param", "token", random_text])
+    captured = run_cli(
+        ["bake", "--local-executor", "seq", "--param", "token", random_text]
+    )
 
     assert "cached" in captured.out
 
@@ -23,6 +27,7 @@ def test_batch_with_local(run_cli: RunCLI) -> None:
     captured = run_cli(
         [
             "bake",
+            "--local-executor",
             "print-local",
             "--param",
             "file_path",
