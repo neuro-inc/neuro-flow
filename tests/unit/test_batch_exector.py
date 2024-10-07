ae64c74e34305c30869eae306a96fd1597984911
Andrew Svetlov <andrew.svetlov@gmail.com>
2024-10-07 11:27:51 +0200

    Drop dead code

--- tests/unit/test_batch_exector.py
+++ tests/unit/test_batch_exector.py
@@ -33,7 +33,6 @@ from typing import (
     Callable,
     Coroutine,
     Dict,
-    Iterable,
     List,
     Mapping,
     Optional,
@@ -51,59 +50,6 @@ from apolo_flow.types import ImageStatus, LocalPath, TaskStatus
 
 
 MakeBatchRunner = Callable[[Path], Awaitable[BatchRunner]]
-
-
-def make_descr(
-    job_id: str,
-    *,
-    status: JobStatus = JobStatus.PENDING,
-    tags: Iterable[str] = (),
-    description: str = "",
-    scheduler_enabled: bool = False,
-    created_at: datetime = datetime.now(timezone.utc),
-    started_at: Optional[datetime] = None,
-    finished_at: Optional[datetime] = None,
-    exit_code: Optional[int] = None,
-    restart_policy: JobRestartPolicy = JobRestartPolicy.NEVER,
-    life_span: float = 3600,
-    name: Optional[str] = None,
-    container: Optional[Container] = None,
-    pass_config: bool = False,
-) -> JobDescription:
-    if container is None:
-        container = Container(RemoteImage("ubuntu"), Resources(100, 0.1))
-
-    return JobDescription(
-        id=job_id,
-        owner="test-user",
-        cluster_name="default",
-        org_name="org",
-        project_name="test-project",
-        status=status,
-        history=JobStatusHistory(
-            status=status,
-            reason="",
-            description="",
-            restarts=0,
-            created_at=created_at,
-            started_at=started_at,
-            finished_at=finished_at,
-            exit_code=exit_code,
-        ),
-        container=container,
-        scheduler_enabled=scheduler_enabled,
-        uri=URL(f"job://default/test-user/{job_id}"),
-        total_price_credits=Decimal("100"),
-        price_credits_per_hour=Decimal("1"),
-        name=name,
-        tags=sorted(
-            list(set(tags) | {"project:test", "flow:batch-seq", f"task:{job_id}"})
-        ),
-        description=description,
-        restart_policy=restart_policy,
-        life_span=life_span,
-        pass_config=pass_config,
-    )
 
 
 class JobsMock:
@@ -244,7 +190,7 @@ class JobsMock:
             id=job_id,
             owner="test-user",
             cluster_name="default",
-            org_name="myorg",
+            org_name="NO_ORG",
             project_name=project_name,
             preset_name=preset_name,
             status=JobStatus.PENDING,
